f61eeaba7f721c698c60e9dc41ca89312cda76bd
Jingying Wang <astro.jywang@gmail.com>
2019-10-22 00:36:14 +0200

    set package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="katcali",
     author="Jingying Wang",
     author_email="astro.jywang@gmail.com",
-    version="1.0",
+    version="0.1.0",
     packages=["katcali"],
     zip_safe=True,
 )
