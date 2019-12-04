869345c040fbed86b9ebbe19c53eef9eba638865
Phil Bull <philbull@gmail.com>
2019-12-04 20:41:27 +0000

    Add utils module

---
+++ katcali/utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+from numpy.polynomial.legendre import legval
+
+KBOLTZ = 1.38e-23 # Boltzmann constant
+CLIGHT = 2.99792485e8 # Speed of light, m/s
+
+def log_normal(x, mu, sigma):
+    return -((x - mu)**2. /(2* sigma**2.)) \
+           - np.log(np.sqrt(2 * np.pi) * np.abs(sigma))
+
+
+def legendre_timeseries(t, coeffs):
+    """
+    Return a Legendre polynomial as a function of time, where the time series 
+    is rescaled to the domain [-1, 1]. 
+    
+    Parameters
+    ----------
+    t : array_like
+        Array of times.
+    
+    coeffs : array_like
+        Coefficients of Legendre polynomial up to some order.
+    
+    Returns
+    -------
+    poly : array_like
+        Legendre polynomial evaluated at each time.
+    """
+    x = (t - t[0]) / (t[-1] - t[0]) * 2 - 1  # rescale time to [-1,1]
+    return legval(x, coeffs)
+    
+
+def valid_filename(filename):
+    """
+    Return a standardised, valid form of the filename, or raise an error if it 
+    is invalid. This function is needed because filenames are often integers, 
+    but need to be passed in as strings. 
+    
+    Parameters
+    ----------
+    filename : str or int
+        Filename to be validated and standardised.
+    
+    Returns
+    -------
+    filename : str
+        Standardised filename as string.
+    """
+    # Check for valid filename types
+    if isinstance(filename, str): return filename
+    if isinstance(filename, (int, float, np.integer, np.float)):
+        return "%s" % filename
+    
+    # Raise error if invalid type found
+    raise TypeError("filename '%s' must be a string or integer" % filename)
