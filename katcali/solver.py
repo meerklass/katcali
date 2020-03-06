73de34cd48e9982aca2ad100a4c3f14e97409f27
Phil Bull <philbull@gmail.com>
2020-03-06 15:49:59 +0200

    autopep8 solver.py

--- katcali/solver.py
+++ katcali/solver.py
@@ -251,8 +251,8 @@ def vis_ndstamp_model(timestamps, params):
     p = params
     vis_ndstamp = np.ma.array(np.zeros_like(timestamps))
     vis_ndstamp[nd_0] = 0
-    vis_ndstamp[nd_1a] = Tnd*ratio
-    vis_ndstamp[nd_1b] = Tnd*(nd_ratio-ratio)  # 1.8s injection
+    vis_ndstamp[nd_1a] = Tnd * ratio
+    vis_ndstamp[nd_1b] = Tnd * (nd_ratio - ratio)  # 1.8s injection
     return vis_ndstamp
 
 
@@ -268,81 +268,38 @@ def cal_gain0(fname, data, ant, pol, flags, ch,
 
     """
     if fname in ['1551055211', '1551037708']:
-        dp_ca, dp_cb, dp_c0a, dp_c1a, dp_c2a, dp_c3a, dp_c4a, dp_c0b, dp_c1b, dp_c2b, dp_c3b, dp_c4b = kl.cal_dp_c(
-            fname, data, ant, pol, flags, ch, dp_tt, dp_ss, ang_deg)
+        dp_ca, dp_cb, dp_c0a, dp_c1a, dp_c2a, dp_c3a, dp_c4a, dp_c0b, dp_c1b, dp_c2b, dp_c3b, dp_c4b = kl.cal_dp_c(fname, data, ant, pol, flags, ch, dp_tt, dp_ss, ang_deg)
+        
         # vis gap for calibrator
         a1 = vis_clean[dp_c0a, ch].min()-vis_clean[dp_ca, ch].min()
+        
         # T model gap for calibrator
-        b1 = T_ptr[dp_ca].max()-T_ptr[dp_ca].min()
+        b1 = T_ptr[dp_ca].max() - T_ptr[dp_ca].min()
+        
         # vis gap for calibrator
-        a2 = vis_clean[dp_c0b, ch].min()-vis_clean[dp_cb, ch].min()
+        a2 = vis_clean[dp_c0b, ch].min() - vis_clean[dp_cb, ch].min()
+        
         # T model gap for calibrator
-        b2 = T_ptr[dp_cb].max()-T_ptr[dp_cb].min()
+        b2 = T_ptr[dp_cb].max() - T_ptr[dp_cb].min()
     else:
         dp_ca, dp_cb, dp_c0a, dp_c1a, dp_c0b, dp_c1b = kl.cal_dp_c(
             fname, data, ant, pol, flags, ch, dp_tt, dp_ss, ang_deg)
+        
         # vis gap for calibrator
-        a1 = vis_clean[dp_c1a, ch].min()-vis_clean[dp_c0a, ch].min()
+        a1 = vis_clean[dp_c1a, ch].min() - vis_clean[dp_c0a, ch].min()
+        
         # T model gap for calibrator
-        b1 = T_ptr[dp_c1a].min()-T_ptr[dp_c0a].min()
+        b1 = T_ptr[dp_c1a].min() - T_ptr[dp_c0a].min()
+        
         # vis gap for calibrator
-        a2 = vis_clean[dp_c1b, ch].min()-vis_clean[dp_c0b, ch].min()
+        a2 = vis_clean[dp_c1b, ch].min() - vis_clean[dp_c0b, ch].min()
+        
         # T model gap for calibrator
-        b2 = T_ptr[dp_c1b].min()-T_ptr[dp_c0b].min()
+        b2 = T_ptr[dp_c1b].min() - T_ptr[dp_c0b].min()
+    
     ga0 = a1 / b1
     gb0 = a2 / b2
+    
     return ga0, gb0
 
 
-'''
-#######only for test
-########################for scan, break bkg for each line##############
-
-def solve_params_smbr(timestamps, vis, ch, ratio0, Tptr, Tnd, func_gt_param0, func_sm_param0,
-                      nd_0, nd_1a, nd_1b, nt_az_edge, error):
-    return opt.fmin_powell(func_obj_smbr, 
-                           x0=list(func_sm_param0)+list(func_gt_param0)+[ratio0], 
-                           args=(timestamps, vis, ch, Tptr, Tnd, nd_0, nd_1a, nd_1b, nt_az_edge, error))
-
-def func_obj_smbr(p, *args):
-    timestamps, vis, ch, Tptr, Tnd, nd_0, nd_1a, nd_1b, nt_az_edge, error=args
-    func_sm_param=p[0:-5]
-    func_gt_param=p[-5:-1]
-    ratio=p[-1]
-    
-    return -calc_logprob_smbr(timestamps, vis, ch, ratio, Tptr, Tnd, func_gt_param, func_sm_param,
-                              nd_0, nd_1a, nd_1b, nt_az_edge, error)
-
-
-
-def calc_logprob_smbr(timestamps, vis, ch, ratio, Tptr, Tnd, func_gt_param, func_sm_param, nd_0, nd_1a, nd_1b, nt_az_edge, error):
-    total_model=calc_total_model_smbr(timestamps, ratio, Tptr, Tnd, func_gt_param, func_sm_param, nd_0, nd_1a, nd_1b,nt_az_edge)
-    result = ma.sum(-(vis[:,ch]-total_model)**2/(2*error**2)-np.log(2*np.pi*error**2)/2.0) 
-    return result
-
-def calc_total_model_smbr(timestamps, ratio, Tptr, Tnd, func_gt_param, func_sm_param, nd_0, nd_1a, nd_1b,nt_az_edge):
-    
-    return gain_poly(timestamps,func_gt_param)*(func_sm_break_long(timestamps, func_sm_param, nt_az_edge)
-                                              +gal[:,ch]+np.ones(len(timestamps))*Tcmb. ###Galactic and CMB added!
-                                              +Tptr
-                                              +vis_ndstamp_model(timestamps, nd_0, nd_1a, nd_1b, Tnd, ratio))
-
-def func_sm_break_long(timestamps, func_sm_param, nt_az_edge):
-    sm_scan=func_sm_break(timestamps, func_sm_param, nt_az_edge)
-    return np.concatenate((np.ones(nt_az_edge[0])*sm_scan[0],
-                                                sm_scan,
-                                                np.ones(len(timestamps)-nt_az_edge[-1])*sm_scan[-1])) 
-
-def func_sm_break(ts, p_sm, nt_az_edge): #ts is full list of timestamps
-    from numpy.polynomial.legendre import legval
-    sm_part=[]
-    
-    n=int(len(p_sm)/(len(nt_az_edge)-1))
-    for i in range(len(nt_az_edge)-1):
-        t_start=ts[nt_az_edge[i]]
-        t_end=ts[nt_az_edge[i+1]] #not included
-        x = (ts[nt_az_edge[i]:nt_az_edge[i+1]] - t_start) / (t_end - t_start) * 2 - 1  # time to [-1,1]
-        sm_part+=list(legval(x, p_sm[n*i:n*(i+1)]))
-    
-    return np.array(sm_part)
-'''
