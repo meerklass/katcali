80d09f02ad4b56372809ac3d71249e39123c679a
Phil Bull <philbull@gmail.com>
2020-03-06 15:03:30 +0200

    autopep8 and add unfilled docstrings

--- katcali/filter.py
+++ katcali/filter.py
@@ -2,34 +2,80 @@ import numpy as np
 from scipy.interpolate import UnivariateSpline
 from astropy.stats import sigma_clip
 
-def deg_filter(dump, ang_deg_list, sigma_level, n_iter):
-    ang_mean=ang_deg_list[dump].mean()
-    ang_std=ang_deg_list[dump].std()
-    dump_iter=list(dump)
-    #print 'deg_filter'
-    print('deg filter start: '+str(ang_mean)+'+/-'+str(ang_std))
+
+def deg_filter(dump, ang_deg_list, sigma_level, n_iter, verbose=True):
+    """
     
+    Parameters
+    ----------
+    dump
+    
+    ang_deg_list
+    
+    sigma_level : float
+        ...
+    
+    n_iter : int
+        ...
+    
+    verbose : bool, optional
+        Whether to print progress messages. Default: False.
+    
+    Returns
+    -------
+    dump_iter : list
+        ...
+    """
+    ang_mean = ang_deg_list[dump].mean()
+    ang_std = ang_deg_list[dump].std()
+    dump_iter = list(dump)
+    if verbose: print('deg filter start: '+str(ang_mean)+'+/-'+str(ang_std))
+
     for n in range(n_iter):
         for i in dump_iter:
-            bias=abs(ang_deg_list[i]-ang_mean)
+            bias = abs(ang_deg_list[i]-ang_mean)
             if bias > sigma_level*ang_std:
                 #print i
                 dump_iter.remove(i)
-        ang_mean=ang_deg_list[dump_iter].mean()
-        ang_std=ang_deg_list[dump_iter].std()
+        ang_mean = ang_deg_list[dump_iter].mean()
+        ang_std = ang_deg_list[dump_iter].std()
         #print str(n)+': '+str(ang_mean)+'+/-'+str(ang_std)
-    print('deg filter end: '+str(ang_mean)+'+/-'+str(ang_std)+'\n')
+    if verbose: print('deg filter end: '+str(ang_mean)+'+/-'+str(ang_std)+'\n')
     return dump_iter
 
-def curve_filter(x,y,sigma=4,k=5):
-    a=[]
-    b=[]
+
+def curve_filter(x, y, sigma=4, k=5):
+    """
+    
+    Parameters
+    ----------
+    x : ??
+        ...
+    
+    y : ??
+        ...
+    
+    sigma : float, optional
+        Default: 4.
+    
+    k : int, optional
+        Default: 5.
+    
+    Returns
+    -------
+    dump_iter : ??
+        ...
+    """
+    a = []
+    b = []
     for i in range(len(x)):
-        if y[i] is not None and np.isnan(y[i]) is not True: 
+        if y[i] is not None and np.isnan(y[i]) is not True:
             a.append(x[i])
             b.append(y[i])
-    a=np.array(a)
-    b=np.array(b)
-    l=UnivariateSpline(a,b,k=k,s=None)(a)
+    a = np.array(a)
+    b = np.array(b)
+    
+    l = UnivariateSpline(a, b, k=k, s=None)(a)
     filtered_data = sigma_clip(b-l, sigma=sigma, iters=5)
-    return a[filtered_data.mask==False] ###output is what want to keep
+    return a[filtered_data.mask == False]  # output is what want to keep
+    
