37185ae994c3f4f61ba3df429d090c35f000b653
Jingying Wang <astro.jywang@gmail.com>
2024-12-08 16:18:30 +0200

    UHF

---
+++ examples/MeerKLASS_UHF/beam_model_UHF/sub_HiRes.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=64000
+#SBATCH --time=20:00:00
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%J.out
+
+export SINGULARITY_SHELL=/bin/bash
+
+singularity exec ~/containers/katcal.sif ./fit_beam_UHF_HiRes.py
+
+
